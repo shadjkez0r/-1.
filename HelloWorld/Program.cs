b598e359a6ebe5ac846d049cd5c3c7623bc32827
Evgeniy Tronin <shadjkez0r48@gmail.com>
2020-12-11 01:30:17 +0400

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name=Console.ReadLine();
+            Console.Write($"Привет, {name}, сегодня ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(DateTime.Now);
+
+            //Задержка
+            Console.ReadKey();
+        }
+    }
+}
